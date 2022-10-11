1fb5332c2216272f4854a94c2f2b0706a8cd3404
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2022-10-10 21:41:55 -0500

    componentes y clear css

---
+++ src/componentes/AddTodo.svelte
@@ -0,0 +1,37 @@
+<script>
+
+import {opc, todos} from "../stores"
+
+  let todo = { id: "", texto: "", estado: false };
+
+  const addTodo = () => {
+    if (!todo.texto.trim()) {
+      console.log("vacio");
+      todo.texto = "";
+      return;
+    }
+
+    // @ts-ignore
+    todo.id = Date.now();
+    todos.add(todo)
+    console.log(todos);
+    todo = { id: "", texto: "", estado: false };
+
+    opc.mostrar({
+        texto: "Se agrego correctamente",
+        color: "bg-primary",
+        estado: true
+    })
+
+    // mostrarToast(, );
+  };
+</script>
+
+<form on:submit|preventDefault={addTodo}>
+  <input
+    type="text"
+    placeholder="Enter para agregar"
+    class="form-control shadow border-0"
+    bind:value={todo.texto}
+  />
+</form>
---
+++ src/componentes/Todo.svelte
@@ -0,0 +1,47 @@
+<script>
+// @ts-nocheck
+
+  import { todos, opc } from "../stores";
+
+  export let item;
+
+  const delTodos = (id) => {
+    todos.delete(id);
+    opc.mostrar({
+      texto: "Se ha borrado",
+      color: "bg-danger",
+      estado: true,
+    });
+    // mostrarToast(, );
+  };
+
+  const updateTodos = (id) => {
+    todos.put(id);
+    opc.mostrar({
+      texto: "Se ha actualizado",
+      color: "bg-warning",
+      estado: true,
+    });
+    // mostrarToast(, );
+  };
+
+  const classEstado = (valor) =>
+    valor ? "btn btn-sm btn-success" : "btn btn-sm btn-warning";
+
+  const classIcono = (valor) =>
+    valor ? "bi bi-arrow-clockwise" : "bi bi-check2";
+</script>
+
+<div class="shadow my-3 p-3 lead">
+  <p class={item.estado ? "text-decoration-line-through" : ""}>
+    {item.texto}
+  </p>
+  
+  <button class="btn {classEstado(item.estado)}" on:click={updateTodos(item.id)}>
+    <i class={classIcono(item.estado)} />
+  </button>
+
+  <button class="btn btn-sm btn-danger" on:click={delTodos(item.id)}>
+    <i class="bi bi-trash" />
+  </button>
+</div>
